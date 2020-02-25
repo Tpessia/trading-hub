f79b39811b56ab981adbf9f34ad8ddb129d17429
Tpessia <thiago.pessia@hotmail.com>
2020-02-25 15:38:30 -0300

    Update Instructions

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Trading Hub
 
-1. Client Start
-2. Service Get Data
-  1. Data from Storage
-  2. Data from API
-3. Service Send Data
-4. Client Send Action
-5. Repeat 3-4 for all data
-6. Service Send Result
+![alt text](https://raw.githubusercontent.com/Tpessia/trading-hub/master/screen_capture.png)
+
+1. Open the Simulator
+2. Select a group of brazilian stocks to simulate, separated by comma (e.g. PETR4, ITUB4)
+3. Select a range to run the strategy
+4. Insert your strategy code using JavaScript (use "loadScript(url)" to inject a dependencie and "console.print(message)" to log on the Console Panel)
+5. Press Start
+6. Check the results on the Results Panel
--- front/src/modules/home/scenes/Home.tsx
+++ front/src/modules/home/scenes/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { Title, Text, Paragraph } = Typography
 
@@ -9,8 +10,27 @@ export default class Home extends React.Component {
             <>
                 <Title level={1}>Home</Title>
                 <Paragraph>
-                    <Text >
-                        At some point there will be some instructions here and a friendly welcome message :)
+                    <Text>
+                        <ol>
+                            <li>
+                                Open the <Link to='/simulator'>Simulator</Link>
+                            </li>
+                            <li>
+                                Select a group of brazilian stocks to simulate, separated by comma (e.g. PETR4, ITUB4)
+                            </li>
+                            <li>
+                                Select a range to run the strategy
+                            </li>
+                            <li>
+                                Insert your strategy code using JavaScript (use "loadScript(url)" to inject a dependencie and "console.print(message)" to log on the Console Panel)
+                            </li>
+                            <li>
+                                Press Start
+                            </li>
+                            <li>
+                                Check the results on the Results Panel
+                            </li>
+                        </ol>
                     </Text>
                 </Paragraph>
             </>
